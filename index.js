a35c24281df8a3e76279e65a4471318594a7efc6
emmanue-ndenga <emmanuel.ndenga@student.moringaschool.com>
2025-03-25 13:59:43 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const name1 = document.querySelector('main');
+name1.remove(); 
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.appendChild(newHeader);
